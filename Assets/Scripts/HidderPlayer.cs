014d147c1667cd5851eea84ecf298fda2e418ded
eslem <j.eslem03@gmail.com>
2019-01-25 22:55:24 +0100

    props, basic sound

---
+++ Assets/Scripts/HidderPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidderPlayer : MonoBehaviour
+{
+    public NoiseManager noiseManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("makeNoise", 2.0f, 5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void makeNoise()
+    {
+        Debug.Log("Haciendo ruido");
+        noiseManager.hearSound(transform.position);
+    }
+}
---
+++ Assets/Scripts/NoiseManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseManager : MonoBehaviour
+{
+
+    public Component obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void positionObject(Vector3 positionSound)
+    {
+        Vector3 difference = positionSound- transform.position;
+        Vector3 directionOnly = difference.normalized;
+        Vector3 pointAlongDirection = transform.position + (directionOnly * 1f);
+        Debug.DrawLine(transform.position, pointAlongDirection, Color.red, 5);
+        obj.transform.position = pointAlongDirection;
+    }
+
+
+    public void hearSound(Vector3 positionSound)
+    {
+        Debug.Log("Escuchando...");
+        positionObject(positionSound);
+    }
+}
