c843cc0882c126da6dcb7ecddef20cfc5dfb2ddb
Eslem <j.eslem03@gmail.com>
2019-01-25 23:12:13 +0100

    game controller

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject gameOverCanvas;
+    public float currentTime;
+    float timeCounter;
+    bool gameOver;
+    public GameObject[] kids;
+    string kidsName;
+    public Button restartButon, exitButton;
+
+    void Awake()
+    {
+        timeCounter = 0;
+        gameOver = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        kids = GameObject.FindGameObjectsWithTag("kid");
+
+        foreach (GameObject counterKids in kids)
+        {
+            kidsName += counterKids.name+", ";
+        }
+
+        Debug.Log("Niños a buscar: " + kidsName);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (gameOver) { } else
+        {
+
+            Debug.Log("Tiempo Restante: " + currentTime);
+            callTime();
+            checkGame();
+        }
+
+        restartButon.onClick.AddListener(() => { RestartGame(); });
+        exitButton.onClick.AddListener(() => { exitGame(); });
+    }
+
+
+    void callTime()
+    {
+        timeCounter += Time.deltaTime;
+        if (timeCounter >= 1)
+        {
+            timeCounter = 0;
+            //Restamos 1 segundo
+            currentTime -= 1;
+            //Mostramos el log           
+        }
+    }
+
+    void checkGame()
+    {
+        if (currentTime <= 0)
+        {
+            gameOverCanvas.SetActive(true);
+            gameOver = true;
+        }
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void exitGame()
+    {
+        SceneManager.LoadScene("Menu Scene");
+    }
+
+
+   
+
+}
