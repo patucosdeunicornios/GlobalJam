be5f99c3d9a8a82a73e09e021104f48308547790
Eslem <j.eslem03@gmail.com>
2019-01-26 19:40:29 +0100

    Basico prototipe props

---
+++ Assets/Scripts/Transform1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transform1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            transform.position = new Vector3(15, 1.73f, 0);
+        }
+    }
+
+
+
+}
